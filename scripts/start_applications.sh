5d5fabd2de5fc200e2a46a1a455221ae0e138a33
cloudwave6.user22 <cloudwave6.user22@cjon.cloud>
2025-07-25 00:51:34 +0000

    update

--- scripts/start_applications.sh
+++ scripts/start_applications.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-streamlit run /root/streamlit-project/main.py --server.port 80 > /dev/null 2> /dev/null < /dev/null &
-python /root/streamlit-project/backend/app.py > /dev/null 2> /dev/null < /dev/null &
+# streamlit run /root/streamlit-project/main.py --server.port 80 > /dev/null 2> /dev/null < /dev/null &
+# python /root/streamlit-project/backend/app.py > /dev/null 2> /dev/null < /dev/null &
+systemctl restart streamlit
+systemctl restart backend
 exit 0
